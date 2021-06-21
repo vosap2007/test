c07060c9a6e8543223f93bdb8a9d71fcebe86d4c
Volodymyr Udovychenko <vosap2007@i.ua>
2021-06-21 13:43:25 +0300

    test 2

---
+++ test2.js
@@ -0,0 +1,34 @@
+/*let number = 0;
+number = Number(prompt('Введите число'));
+
+let power = 0;
+power = Number(prompt('Введите степень'));
+
+let result = number ** power;
+
+console.log(result);*/
+
+
+
+/*const max = color;
+const min = 22;
+
+const result = Math.round(Math.random() * (max-min) + min);
+
+console.log(result);*/
+
+
+
+/*const colors = ['tomato', 'green', 'write', 'black', 'orange', 'green'];
+const max = colors.length - 1;
+const min = 0;
+
+const index = Math.round(Math.random() * (max - min) + min);
+
+const color = colors[index];
+
+console.log(color);
+
+document.body.style.backgroundColor = color;*/
+
+
