ca50d08020bb64407443cc9535208db1282d02b3
Volodymyr Udovychenko <vosap2007@i.ua>
2021-07-04 23:04:29 +0300

    4 конспект

---
+++ test4.js
@@ -0,0 +1,43 @@
+// const functionCallback = function (string) {
+//     console.log(string);
+// };
+
+// const higherOrderFunction = function (functionCallback) {
+//     const message = 'Hello word!';
+
+//     functionCallback(message);
+// };
+
+// higherOrderFunction(functionCallback);
+
+
+// const repeatLog = function (n) {
+//     for (let i = 0; i < n; i += 1) {
+//         console.log(i);
+//     };
+// };
+
+// repeatLog(5);
+
+
+
+// const outputsCleanData = function(data) {
+//     console.log(data);
+// };
+
+// const outputsDataWithSignature = function(data) {
+//     console.log('Это номер строки массива', data);
+// };
+
+// const higherOrderFunction = function (n, callbeck) {
+
+//     for (let i = 0; i < n; i +=1) {
+//         callbeck(i);
+//     };
+// };
+
+// higherOrderFunction(3, outputsCleanData);
+// higherOrderFunction(5, outputsDataWithSignature);
+
+
+
